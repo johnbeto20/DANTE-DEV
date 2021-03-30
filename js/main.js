aacbcb8644a81c513fecd482e4fb6b468dab89e5
john sanchez <john.sanchez@ceiba.com.co>
2021-03-29 23:35:46 -0500

    subir responsive y otros ajustes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', function(e) {
 
     const Elements = document.getElementById("elements").getElementsByTagName('li');
+    const ounboarding = document.getElementById("ounboardingText");
     const Modal = document.getElementById("modal");
     const CloseModal = document.getElementById("closeModal");
     const ModalContent = document.getElementById("modalContent");
@@ -9,12 +10,14 @@ window.addEventListener('load', function(e) {
     const ModalIcon = document.getElementById("modalIcon");
     const ModalEpigrafe = document.getElementById("modalEpigrafe");
     const modalImagenText = document.getElementById("modalImagenText");
+    const ModalImagenUrl = document.getElementById("modalImagenUrl");
     const ModalImage = document.getElementById("modalImage");
     const ModalText1 = document.getElementById("modalText1");
     const ModalTitleAbout = document.getElementById("modalTitleAbout");
     const ModalText2 = document.getElementById("modalText2");
     const BtnStart = document.getElementById("btnStart");
     const Section1 = document.getElementById("section1");
+    const ModalText = document.getElementById("modalText");
 
     BtnStart.addEventListener("click", function() {
         Section1.classList.add("hideIntro");
@@ -24,8 +27,14 @@ window.addEventListener('load', function(e) {
         Modal.classList.remove("show");
     })
 
+    Elements[0].classList.add("inicial");
+
     for (let index = 0; index < Elements.length; index++) {
         Elements[index].addEventListener("click", function() {
+            if (index == 0) {
+                this.classList.remove("inicial");
+                ounboarding.style.display = "none";
+            }
             showInfo(index);
             Modal.classList.add("show");
         });
@@ -45,13 +54,17 @@ window.addEventListener('load', function(e) {
             if (this.readyState == 4 && this.status == 200) {
                 let datos = JSON.parse(this.responseText);
                 //traer datos
+                ModalText.scrollTo(0, 0);
+                Modal.scrollTo(0, 0);
                 ModalContent.setAttribute("data-status", datos[e].status)
                 ModalStatus.innerHTML = datos[e].status;
                 ModalTitle.innerHTML = datos[e].title;
                 ModalIcon.src = `img/momento-${e}-modal.png`;
                 ModalEpigrafe.innerHTML = datos[e].epigrafe;
                 ModalImage.src = `img/gallery/${datos[e].image}`;
+                ModalImage.setAttribute("alt", datos[e].imagenText);
                 modalImagenText.innerHTML = datos[e].imagenText;
+                ModalImagenUrl.innerHTML = datos[e].imagenUrl;
                 ModalText1.innerHTML = datos[e].content.text1;
                 ModalTitleAbout.innerHTML = datos[e].content.titleAbout;
                 ModalText2.innerHTML = datos[e].content.text2;
